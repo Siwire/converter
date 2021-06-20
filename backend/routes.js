a4b8ea8bf58a4624ab707faa2e47412a47bd1c9e
Siwire <kholyavo.ivan@gmail.com>
2021-06-20 20:46:43 +0300

    backend code refactoring

--- backend/routes.js
+++ backend/routes.js
@@ -10,69 +10,51 @@ const convertToBYN = (valueCurr, currRate, currScale) => {
 const defaultCurrencies = ["USD", "EUR", "RUB"];
 module.exports = function (app) {
     app.post('/get_new_currencies', async (req, res) => {
-        const selectedCurrencies = req.body.selectedCurrencies
-        const currencies = await axios.get("https://www.nbrb.by/API/ExRates/Rates?Periodicity=0")
+        const selectedCurrencies = req.body;
+        const currencies = await axios.get("https://www.nbrb.by/API/ExRates/Rates?Periodicity=0");
         const getNewCurrencies = currencies.data.filter(currency => {
             if (!(selectedCurrencies.find(selectedCurrency => selectedCurrency.Cur_Abbreviation === currency.Cur_Abbreviation))) {
-                return currency
+                return currency;
             }
+        });
+        res.send(getNewCurrencies);
+    });
 
-        })
-        res.send(getNewCurrencies)
-    })
     app.post('/get_currencies', async (req, res) => {
-        const startCurrencies = defaultCurrencies
         const currencies = await axios.get("https://www.nbrb.by/API/ExRates/Rates?Periodicity=0")
-        const getDefaultCurrencies = currencies.data.filter(currency => {
-            if (startCurrencies.find(startCurrency => startCurrency === currency.Cur_Abbreviation)) {
-                return currency
-            }
-        })
-        const currUSD = getDefaultCurrencies.find(currency => {
-            if (currency.Cur_Abbreviation === "USD") {
-                return currency
-            }
-        })
+        const getDefaultCurrencies = currencies.data.filter(currency => defaultCurrencies.find(startCurrency => startCurrency === currency.Cur_Abbreviation));
+        const currencyUSD = getDefaultCurrencies.find((currency) => currency.Cur_Abbreviation === "USD");
+        const defaultUSDInBYN = currencyUSD.Cur_OfficialRate;
 
-        const valueBYN = +currUSD.Cur_OfficialRate
-        const getRateCurrencies = getDefaultCurrencies.map(currency => {
-            const value = convert(valueBYN, currency.Cur_OfficialRate, currency.Cur_Scale)
-            const roundValue = (value) => {
-                return +value.toFixed(4);;
-            }
-            return { ...currency, Cur_Value: roundValue(value) }
-        })
-        getRateCurrencies.push({ Cur_Abbreviation: "BYN", Cur_Name: "Белорусский рубль", Cur_Value: valueBYN, Cur_OfficialRate: 1, Cur_Scale: 1 });
-        res.send(getRateCurrencies)
+        const response = getDefaultCurrencies.map((defaultCurrency) => {
+            const value = convert(defaultUSDInBYN, defaultCurrency.Cur_OfficialRate, defaultCurrency.Cur_Scale);
+            return { ...defaultCurrency, value: +value.toFixed(4) };
+        });
+        response.push({ Cur_Abbreviation: "BYN", Cur_Name: "Белорусский рубль", value: defaultUSDInBYN, Cur_OfficialRate: 1, Cur_Scale: 1 });
+        res.send(response);
+    });
 
-    })
     app.post('/cur_rate', async (req, res) => {
-        const selectedCurrencies = req.body.currencies
-        const changedCurrencyName = req.body.target
-        const changedCurrency = selectedCurrencies.find(currency => {
-            if (currency.Cur_Abbreviation === changedCurrencyName) {
-                return currency
-            }
-        })
-        const valueBYN = convertToBYN(changedCurrency.Cur_Value, changedCurrency.Cur_OfficialRate, changedCurrency.Cur_Scale)
-        const getNewValueCurrencies = selectedCurrencies.map(currency => {
-            const value = convert(valueBYN, currency.Cur_OfficialRate, currency.Cur_Scale)
-            const roundValue = (value) => {
-                return +value.toFixed(4);
-            }
-            return { ...currency, Cur_Value: roundValue(value) }
-        })
-        const changeValueBYN = getNewValueCurrencies.find(currency => currency.Cur_Abbreviation === "BYN").Cur_Value = valueBYN
-        res.send(getNewValueCurrencies)
-    })
-    app.post('/add_currency', async (req, res) => {
-        const currencyBYN = req.body.currencyBYN
-        const newCurrency = req.body.newCurrency
+        const { target, value, currencies } = req.body;
+        const changedCurrency = currencies.find((cur) => cur.Cur_Abbreviation === target);
+        const changedCurrencyInBYN = changedCurrency.Cur_OfficialRate * value / changedCurrency.Cur_Scale;
 
-        const value = convert(currencyBYN.Cur_Value, newCurrency.Cur_OfficialRate, newCurrency.Cur_Scale)
-        const roundValue = (value) => {
-            return +value.toFixed(4);
-        }
-        res.send({ ...newCurrency, Cur_Value: roundValue(value) })
-    })
+        const result = currencies.map((currency) => {
+            const { Cur_Scale } = currency;
+            if (currency.Cur_Abbreviation === target) {
+                return { ...currency, value };
+            }
+            const currencyValue = changedCurrencyInBYN / currency.Cur_OfficialRate;
+            return { ...currency, value: (parseFloat(currencyValue) * Cur_Scale).toFixed(4) };
+        });
+
+        res.send(result);
+    });
+
+    app.post('/add_currency', async (req, res) => {
+        const { newCurrency, currencyBYNValue } = req.body;
+        const value = convert(currencyBYNValue, newCurrency.Cur_OfficialRate, newCurrency.Cur_Scale)
+
+        res.send({ ...newCurrency, value: +value.toFixed(4) });
+    });
 }
