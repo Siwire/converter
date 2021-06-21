e477fca8c280cb90d93bb84fed0715d2655cfb00
Siwire <kholyavo.ivan@gmail.com>
2021-06-21 11:29:13 +0300

    fix url

--- client/src/redux/modules/currencies.js
+++ client/src/redux/modules/currencies.js
@@ -7,11 +7,13 @@ import {
 	CHANGE_VALUE, HANDLE_CHANGE_VALUE, FETCH_ADD_NEW_CURRENCY, ADD_CURRENCY
 } from '../converterTypes';
 
+const url = 'http://localhost:8000/';
+
 const fetchCurrenciesFulfilled = payload => ({ type: CURRENCIES, payload });
 
 export const fetchCurrenciesEpic = action$ => action$.pipe(
 	ofType(FETCH_CURRENCIES),
-	mergeMap(action => ajax.post(`http://localhost:8000/get_currencies`).pipe(
+	mergeMap(action => ajax.get(`${url}get_currencies`).pipe(
 		map(response => fetchCurrenciesFulfilled(response))
 	))
 );
@@ -20,7 +22,7 @@ const fetchNewCurrenciesFulfilled = payload => ({ type: NEW_CURRENCIES, payload 
 
 export const fetchNewCurrenciesEpic = action$ => action$.pipe(
 	ofType(FETCH_NEW_CURRENCIES),
-	mergeMap(action => ajax.post(`http://localhost:8000/get_new_currencies`, action.payload, { 'Content-Type': 'application/json' }).pipe(
+	mergeMap(action => ajax.post(`${url}get_new_currencies`, action.payload, { 'Content-Type': 'application/json' }).pipe(
 		map(response => fetchNewCurrenciesFulfilled(response))
 	))
 );
@@ -31,7 +33,7 @@ export const changeInputValueCurrencyEpic = action$ => action$.pipe(
 	ofType(HANDLE_CHANGE_VALUE),
 	mergeMap(action => concat(
 		of({ type: CHANGE_VALUE, payload: action.payload }),
-		from(ajax.post(`http://localhost:8000/cur_rate`, action.payload, { 'Content-Type': 'application/json' })).pipe(
+		from(ajax.post(`${url}cur_rate`, action.payload, { 'Content-Type': 'application/json' })).pipe(
 			map(response => fetchNewValueCurrenciesFulfilled(response)),
 		)
 	)),
@@ -41,7 +43,7 @@ const fetchAddNewCurrencyFulfilled = payload => ({ type: ADD_CURRENCY, payload }
 
 export const addNewCurrencyFulfilledEpic = action$ => action$.pipe(
   ofType(FETCH_ADD_NEW_CURRENCY),
-  mergeMap(action => ajax.post(`http://localhost:8000/add_currency`, action.payload, { 'Content-Type': 'application/json' }).pipe(
+  mergeMap(action => ajax.post(`${url}add_currency`, action.payload, { 'Content-Type': 'application/json' }).pipe(
     map(response => fetchAddNewCurrencyFulfilled(response))
   ))
 );
